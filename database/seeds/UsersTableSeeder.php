98ea78ffc845c6fd723a46666f51b777a07ad25b
daniel.meneses <daniel.meneses@cuemby.com>
2019-11-25 02:30:46 -0500

    add users seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            "name" => 'Admin',
+            "password" => Hash::make('password'),
+            "email" => 'admin@mail.com',
+        ];
+
+        User::insert($user);
+    }
+}
