afddbeafb2e9546f56ce67e66508061f17d7a8cd
daniel.meneses <daniel.meneses@cuemby.com>
2019-11-27 10:25:34 -0500

    add laravel mix configuration to correctly compile vue components

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -2,35 +2,35 @@ export const routes = [
     {
         path: '/login',
         name: 'login',
-        component: () => import( /* webpackChunkName: "login" */ '../components/auth/Login'),
+        component: () => import( /* webpackChunkName: "login" */ '~/components/auth/Login.vue'),
         layout: 'defaultLayout',
         isPublic: true,
     },
     {
         path: '/home',
         name: 'home',
-        component: () => import( /* webpackChunkName: "home" */ '../components/Home'),
+        component: () => import( /* webpackChunkName: "home" */ '~/components/Home.vue'),
         layout: 'appLayout',
         isPublic: false,
     },
     {
         path: '/companies',
         name: 'companies',
-        component: () => import( /* webpackChunkName: "companies" */ '../components/companies/Companies'),
+        component: () => import( /* webpackChunkName: "companies" */ '~/components/companies/Companies.vue'),
         layout: 'appLayout',
         isPublic: false,
     },
     {
         path: '/employees',
         name: 'employees',
-        component: () => import( /* webpackChunkName: "employees" */ '../components/employees/Employees'),
+        component: () => import( /* webpackChunkName: "employees" */ '~/components/employees/Employees.vue'),
         layout: 'appLayout',
         isPublic: false,
     },
     {
         path: '*',
         name: 'not_found',
-        component: () => import( /* webpackChunkName: "404" */ '../components/commons/404'),
+        component: () => import( /* webpackChunkName: "404" */ '~/components/commons/404.vue'),
         layout: 'defaultLayout',
         isPublic: true
     }
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,33 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css')
+   .sourceMaps()
    .setResourceRoot('../');
+
+   if (mix.inProduction()) {
+    mix.version()
+
+    mix.extract([
+      'vue',
+      'vform',
+      'axios',
+      'vuex',
+      'vue-i18n',
+      'vue-meta',
+      'js-cookie',
+      'vue-router',
+      'vuetify',
+      'vee-validate',
+      'vuex-router-sync'
+    ])
+  }
+
+  mix.webpackConfig({
+    plugins: [
+    ],
+    resolve: {
+      alias: {
+        '~': path.join(__dirname, './resources/js')
+      }
+    }
+  })
