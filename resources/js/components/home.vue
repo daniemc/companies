48ef57917242165596c37ffd87899ec9eddaca2c
daniel.meneses <daniel.meneses@cuemby.com>
2019-11-27 12:24:29 -0500

    add page header to home

--- resources/js/components/home.vue
+++ resources/js/components/home.vue
@@ -1,12 +1,25 @@
 <template>
     <div>
-        Hey, logued in!!!
+        <page-header :pageName="pageName" :pageBreadCrumb="pageBreadCrumb"></page-header>
+        <div>
+            <span>
+                Hey, logued in!!!
+            </span>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         name: 'home-view',
+        data() {
+            return {
+                pageName: 'Home',
+                pageBreadCrumb: [
+                    { name: 'Home', active: true, url: '/home' },
+                ],
+            }
+        },
     }
 </script>
 
